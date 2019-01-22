42c7f05c8c3d4b29d5bfdfa97c526513eb15bb83
Ean Daus <eandaus123@gmail.com>
2019-01-22 15:06:07 -0500

    added files for part 2 of the review assignment

---
+++ js-review-2.js
@@ -0,0 +1,26 @@
+
+
+
+//test the functions
+var obj = {here: {is: "an"}, object: 2};
+
+console.log(objEquals(obj, obj));
+// → true
+
+console.log(objEquals(obj, {here: 1, object: 2}));
+// → false
+
+console.log(objEquals(obj, {here: {is: "an"}, object: 2}));
+// → true
+
+console.log(objEquals(obj, {here: {is: "an"}, object: 0}));
+// → false
+
+console.log(objEquals(obj, {here: {is: "another"}, object: 2}));
+// → false
+
+console.log(objEquals(obj, {here: {isnt: "an"}, object: 2}));
+// → false
+
+console.log(objEquals(obj, {here: {is: "an", deep: {poop: null, value: -1}}, object: 2}));
+// → false
